10cbb5dcbbd2a18f80cdc97c233296afbac7764d
Do Trinh <trinh.thanh.do@use-inc.co.jp>
2021-04-15 12:36:33 +0900

    bai 1

---
+++ app.js
@@ -0,0 +1,7 @@
+//https://nodejs.org/en/download
+//install
+//confirm: node -v
+//make a dir in Desktop: nodejs
+//create a file: app.js
+//run: node app.js
+console.log('Nodejs with dotrinh.com');
